2d17ac2cee9326da701e60b5801029cb42c4b8c2
Lonely <15892222603@163.com>
2019-03-21 10:24:16 +0800

    配置了项目打包为war包

--- src/main/java/com/lonely/springboot/SpringbootApplication.java
+++ src/main/java/com/lonely/springboot/SpringbootApplication.java
@@ -7,12 +7,14 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.http.HttpMessageConverters;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
 @MapperScan("com.lonely.springboot.dao")
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
@@ -32,5 +34,10 @@ public class SpringbootApplication {
 		fasHttpMessageConverter.setFastJsonConfig(fastJsonConfig);
 		HttpMessageConverter<?> converter = fasHttpMessageConverter;
 		return new HttpMessageConverters(converter);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(SpringbootApplication.class);
 	}
 }
